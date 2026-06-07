9f1d12127e187ee53fb3a25d38cb12107de177e5
Lucas Gomes <>
2026-06-07 19:39:17 -0300

    Criando primeiro teste unitario.

---
+++ GettingStarted/GettingStarted/GettingStartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GettingStartedApp.swift
+//  GettingStarted
+//
+//  Created by Lucas Gomes on 07/06/26.
+//
+
+import SwiftUI
+
+@main
+struct GettingStartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
